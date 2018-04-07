5defb02e7cf08137e9d7f604efa5d4c786056a8f
Sargis Sargsyan <sargis@Sargiss-MacBook-Pro.local>
2018-04-07 17:18:34 +0400

    form submit fixings

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,28 @@
-'use strict'
-const functions = require('firebase-functions')
-const nodemailer = require('nodemailer')
-const cors = require('cors')({origin: true})
+"use strict";
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const cors = require("cors")({ origin: true });
 
 exports.contactMe = functions.https.onRequest((req, res) => {
+  console.log(req);
   cors(req, res, () => {
-    sendEmail(req.body)
-    res.status(200).send({})
-  })
-})
+    sendEmail(req.body);
+    res.status(200).send({});
+  });
+});
 
-function sendEmail (params) {
-  const config = functions.config()
-  const auth = config.auth
-  const mail = config.mail
+function sendEmail(params) {
+  console.log(params);
+  if (!params.name || !params.email || !params.message) {
+    return;
+  }
+  const config = functions.config();
+  const auth = config.auth;
+  const mail = config.mail;
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: "Gmail",
     auth: {
-      type: 'OAuth2',
+      type: "OAuth2",
       user: auth.user,
       clientId: auth.client_id,
       clientSecret: auth.client_secret,
@@ -25,14 +30,13 @@ function sendEmail (params) {
       accessToken: auth.access_token,
       expires: auth.expires
     }
-  })
+  });
 
   const mailOptions = {
     from: mail.from,
     to: mail.to,
     subject: `Website Contact Form:  ${params.name}`,
-    text:
-      `You have received a new message from your website contact form.
+    text: `You have received a new message from your website contact form.
       
     Here are the details:
     
@@ -45,13 +49,12 @@ function sendEmail (params) {
     Message:
     
       ${params.message}`
-  }
-
+  };
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      return console.log(error)
+      return console.log(error);
     }
-    console.log('Message sent: %s', info.messageId)
-  })
+    console.log("Message sent: %s", info.messageId);
+  });
 }
