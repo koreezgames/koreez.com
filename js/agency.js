472b16632ba2de0869fe8c8da81324218123394c
Artur Vardanyan <a.vardanyan1997@gmail.com>
2019-04-25 13:11:34 +0400

    header update

--- js/agency.js
+++ js/agency.js
@@ -3,27 +3,34 @@
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 54
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 55
   });
 
   // Collapse the navbar when page is scrolled
@@ -35,6 +42,5 @@
     }
   });
 
-  $('#year').text(new Date().getFullYear())
-
+  $("#year").text(new Date().getFullYear());
 })(jQuery); // End of use strict
